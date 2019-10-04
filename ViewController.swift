5fb3d6aea472fab9aa175335b1e587995832c725
THitokuse <hasegawa.taichiro@gmail.com>
2019-10-05 02:09:36 +0900

    create FoodTracker

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 長谷川太一朗 on 2019/10/04.
+//  Copyright © 2019 hasegawa taichiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
