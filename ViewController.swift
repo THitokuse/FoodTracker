95e68fee3a38735cfece58b0dafd19e79ae23363
THitokuse <hasegawa.taichiro@gmail.com>
2019-10-05 12:08:22 +0900

    storyboardでUIを作成

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  // MARK: Properties
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var mealNameLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
+    // Handle the text field's user input through delegate callbacks.
+    nameTextField.delegate = self
+  }
 
+  // MARK: Action
+  @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    mealNameLabel.text = "Default Text"
+  }
 }
 
+// MARK: UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard.
+    textField.resignFirstResponder()
+    return true
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    mealNameLabel.text = textField.text
+  }
+}
